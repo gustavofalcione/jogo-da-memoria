361c09fc9f5688f8303cab2e4cf1c5d3476e81a0
Gustavo <falcionegustavo@gmail.com>
2022-03-15 12:58:43 -0300

    criando tabuleiro

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 const FRONT = "card_front";
 const BACK = "card_back";
+const CARD = "card";
+const ICON = "icon";
 
 let techs = [
   "bootstrap",
@@ -11,6 +13,7 @@ let techs = [
   "mongo",
   "node",
   "react",
+  "css",
 ];
 
 let cards = null;
@@ -20,6 +23,43 @@ startGame();
 function startGame() {
   cards = createCardsFromTechs(techs);
   shuffleCards(cards);
+
+  initializeCards(cards);
+}
+
+function initializeCards(cards) {
+  let gameBoard = document.getElementById("gameBoard");
+
+  cards.forEach((card) => {
+    let cardElement = document.createElement("div");
+    cardElement.id = card.id;
+    cardElement.classList.add(CARD);
+    cardElement.dataset.icon = card.icon;
+
+    createCardContent(card, cardElement);
+
+    cardElement.addEventListener("click", flipCard);
+    gameBoard.appendChild(cardElement);
+  });
+}
+
+function createCardContent(card, cardElement) {
+  createCardFace(FRONT, card, cardElement);
+  createCardFace(BACK, card, cardElement);
+}
+
+function createCardFace(face, card, element) {
+  let cardElementFace = document.createElement("div");
+  cardElementFace.classList.add(face);
+  if (face === FRONT) {
+    let iconElement = document.createElement("img");
+    iconElement.classList.add(ICON);
+    iconElement.src = "./assets/images/" + card.icon + ".png";
+    cardElementFace.appendChild(iconElement);
+  } else {
+    cardElementFace.innerHTML = "&lt/&gt";
+  }
+  element.appendChild(cardElementFace);
 }
 
 function shuffleCards(cards) {
@@ -40,9 +80,9 @@ function shuffleCards(cards) {
 function createCardsFromTechs(techs) {
   let cards = [];
 
-  for (let tech of techs) {
+  techs.forEach((tech) => {
     cards.push(createPairFromTech(tech));
-  }
+  });
 
   return cards.flatMap((pair) => pair);
 }
@@ -64,4 +104,8 @@ function createPairFromTech(tech) {
 
 function createIdWithTech(tech) {
   return tech + parseInt(Math.random() * 1000);
+}
+
+function flipCard() {
+  this.classList.add("flip");
 }
